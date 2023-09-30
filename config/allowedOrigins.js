19cd0322ada69abda0a67b3834e2c370b02a01cc
Harikrishna3 <harikrishnabbomen@gmail.com>
2023-09-30 09:49:41 +0530

    Implimented API endpoints

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://jade-trifle-e12ea5.netlify.app/',
+    'https://www.jade-trifle-e12ea5.netlify.app/'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors(corsOptions))
 
 // Sample data for doctors
 const doctors = [
