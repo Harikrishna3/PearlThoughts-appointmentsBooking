6354f5ffd444ee209fbb5a079ecac7e5cfcaab63
Harikrishna3 <harikrishnabbomen@gmail.com>
2023-10-05 20:14:03 +0530

    r

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://main--jade-trifle-e12ea5.netlify.app/api/doctors/',
-    'https://www.jade-trifle-e12ea5.netlify.app/'
+    'https://www.jade-trifle-e12ea5.netlify.app/',
+    'https://pearlthoughts-api.onrender.com',
 ]
 
 module.exports = allowedOrigins
